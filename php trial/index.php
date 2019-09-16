1a511614e8495c17dc0e7adece3e9a90d2e73058
A-Shafei <50213577+A-Shafei@users.noreply.github.com>
2019-09-16 12:35:30 +0200

    Add files via upload

---
+++ php trial/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <title>A. Shafey PHP</title>
+</head>
+<body>
+  <h1>A. Shafey PHP</h1>
+  <pre>
+               ******
+              ********
+             ***    ***
+            ***      ***
+           ***        ***
+          ***          ***
+         ***            ***
+        ********************
+       **********************
+      ***                  ***
+     ***                    ***
+    ***                      ***
+
+  </pre>
+  <?php
+  print hash('sha256', 'A. Shafey')
+  ?>
